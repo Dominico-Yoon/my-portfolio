9a3620248516d9b3a010898adefe1b5608dc6333
Dominico Yoon <808dominico@gmail.com>
2024-08-07 21:03:39 +0900

    20240807 - feat: todoApp 추가, 삭제 기능 구현

--- app/todoApp/page.tsx
+++ app/todoApp/page.tsx
@@ -1,14 +1,74 @@
+"use client";
+
+import { ChangeEvent, useState } from "react";
+
+interface Data {
+  id: number;
+  todo: string;
+  isDone: boolean;
+}
+
+const mockData: Data[] = [
+  {
+    id: 0,
+    todo: "공부하기",
+    isDone: true,
+  },
+  {
+    id: 1,
+    todo: "청소하기",
+    isDone: false,
+  },
+  {
+    id: 2,
+    todo: "빨래하기",
+    isDone: false,
+  },
+];
+
 const TodoApp = () => {
+  const [todos, setTodos] = useState<Data[]>(mockData);
+  const [newTodo, setNewTodo] = useState<string>("");
+
+  const addTodo = () => {
+    if (newTodo.trim()) {
+      setTodos((prevTodos) => [
+        ...prevTodos,
+        { id: prevTodos.length, todo: newTodo, isDone: false },
+      ]);
+      setNewTodo("");
+    }
+  };
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setNewTodo(e.target.value);
+  };
+
+  const deleteTodo = (targetId: number) => {
+    setTodos(todos.filter((item) => item.id !== targetId));
+  };
+
   return (
     <div>
       <div>
-        <input type="text" placeholder="새 할일을 입력하세요." />
-        <button>추가</button>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={handleInputChange}
+          placeholder="새 할일을 입력하세요."
+        />
+        <button onClick={addTodo}>추가</button>
       </div>
       <div>
-        <li>할일 1</li>
-        <li>할일 2</li>
-        <li>할일 3</li>
+        {todos.map((item) => {
+          return (
+            <div key={item.id}>
+              <input type="checkbox" />
+              <div>{item.todo}</div>
+              <button onClick={() => deleteTodo(item.id)}>삭제</button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
