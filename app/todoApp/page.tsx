e172b4a9ca4ec90e02eeebcb3ad891a18f9dfa6d
Dominico Yoon <808dominico@gmail.com>
2024-08-07 16:56:21 +0900

    20240807 - feat: todoApp 기초틀 생성

--- app/todoApp/page.tsx
+++ app/todoApp/page.tsx
@@ -1,5 +1,17 @@
 const TodoApp = () => {
-  return <div>TodoApp</div>;
+  return (
+    <div>
+      <div>
+        <input type="text" placeholder="새 할일을 입력하세요." />
+        <button>추가</button>
+      </div>
+      <div>
+        <li>할일 1</li>
+        <li>할일 2</li>
+        <li>할일 3</li>
+      </div>
+    </div>
+  );
 };
 
 export default TodoApp;
